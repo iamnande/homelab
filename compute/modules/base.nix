c6516a3738faaebc19bcd5d9b3a3e9a50934af5c
Nick Anderson <nick@morethq.com>
2026-03-22 21:44:28 -0700

    feat(compute): base node configuration

---
+++ compute/modules/base.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  # boot
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # network
+  networking.hostName = "ifrit";
+  networking.networkmanager.enable = true;
+
+  # locale
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # user
+  users.users.nick = {
+    isNormalUser = true;
+    description = "nick";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
+  };
+
+  # packages
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    btop
+    curl
+    fd
+    fish
+    fishPlugins.tide
+    git
+    gnumake
+    helix
+    htop
+    jq
+    lsof
+    neofetch
+    nh
+    nix-tree
+    ripgrep
+    stow
+    tree
+    unzip
+    wget
+    zellij
+  ];
+
+  # programs
+  programs.fish.enable = true;
+
+  # services
+  services.openssh.enable = true;
+
+  # nix
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "25.11";
+}
